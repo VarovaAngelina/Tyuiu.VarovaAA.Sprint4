46354d309c50b2961c1590153c2aa150e467faaa
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-11-05 18:19:13 +0500

    Task3

---
+++ Tyuiu.VarovaAA.Sprint4.Task3.V17.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.VarovaAA.Sprint4.Task3.V17.Lib
+{
+    public class DataService : ISprint4Task3V17
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+
+            int res = 0;
+
+            for (int j = 0; j < columns; j++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    if (j == 3)
+                    {
+                        res += array[i, j];
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.VarovaAA.Sprint4.Task3.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VarovaAA.Sprint4.Task3.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VarovaAA.Sprint4.Task3.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ffb0c23c-8a84-4482-bce3-4f4761613559")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
