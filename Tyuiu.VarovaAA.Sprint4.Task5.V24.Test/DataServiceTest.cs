4106469476b008dd14cb2fefc739c0f3b9542a43
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-11-12 16:17:54 +0500

    Task5

---
+++ Tyuiu.VarovaAA.Sprint4.Task5.V24.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.VarovaAA.Sprint4.Task5.V24.Lib;
+namespace Tyuiu.VarovaAA.Sprint4.Task5.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { -7, -6, -1, -5, 1 },
+                                          { -1, 1, -1, -1, 6 },
+                                          { -1, 2, 3, 1, -1 },
+                                          { 1, -1, -1, -1, -1 },
+                                          { 1, 0, -1, 1, -1} };
+            int res = ds.Calculate(mas2);
+            int wait = 9;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VarovaAA.Sprint4.Task5.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VarovaAA.Sprint4.Task5.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VarovaAA.Sprint4.Task5.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ea90b79-3361-45ad-b58f-48352f06b89d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
