a5113996fa305b2acabb6dc039ab8d7cced1ae0b
Tyuiu.VarovaAA@outlook.com <FoxHighlight@yandex.ru>
2023-11-12 21:28:52 +0500

    Task7

---
+++ Tyuiu.VarovaAA.Sprint4.Task7.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VarovaAA.Sprint4.Task7.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VarovaAA.Sprint4.Task7.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("140ab928-0da6-44c6-9693-90b247a5b754")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
